d1488b0822f118582335e9de6811e362da94b628
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2020-06-09 23:06:54 +0900

    :white_check_mark: Fix dependency in spec of NgxDialogService

--- projects/ngx-dialog/src/lib/ngx-dialog.service.spec.ts
+++ projects/ngx-dialog/src/lib/ngx-dialog.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NgxDialogService } from './ngx-dialog.service';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 describe('NgxDialogService', () => {
   let service: NgxDialogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [OverlayModule]
+    });
     service = TestBed.inject(NgxDialogService);
   });
 
